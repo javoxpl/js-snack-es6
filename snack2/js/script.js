33ff7da9e9814bae4eb66082be0d374d255c5c65
javoxpl <liacovone7@outlook.it>
2022-06-06 14:12:02 +0200

    teams

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -3,3 +3,39 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+var squadre = 
+[
+        {
+            nome : "inter",
+            puntiFatti : 0,
+            falliSubiti : 0,
+        },
+
+        {
+            nome: "Real Sociedad",
+            puntiFatti: 0,
+            falliSubiti: 0,
+        },
+
+        {
+            nome: "Paris saint-germain",
+            puntiFatti: 0,
+            falliSubiti: 0,
+        },
+
+        {
+            nome: "allas verona",
+            puntiFatti: 0,
+            falliSubiti: 0,
+        },
+        
+]
+
+function casualPunti(){
+ return   Math.floor(Math.random()*40 + 7)
+}
+
+function casualFalli() {
+    return Math.floor(Math.random() * 15 + 25)
+}
