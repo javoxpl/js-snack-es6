639ca6f6391a493ac7d95a6fc5c34c6ef49b66e6
javoxpl <liacovone7@outlook.it>
2022-06-07 16:14:45 +0200

    filter

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -3,3 +3,24 @@ const myArray = [‘Michele’, ‘Fabio’, ‘Roberto’, ‘Giovanni’, ‘S
 e dati due numeri min e max (min più piccolo di max).
 Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
 Eseguiamo questo esercizio prima con forEach e poi con filter.*/
+
+
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+const nuovoArray = [];
+
+const primoNumero = parseInt(prompt('inserisci un numero'))
+const secondoNumero = parseInt(prompt('inserisci un numero'))
+
+
+// esempio con .filter
+
+
+const ultimoArray = myArray.filter((element, index) => {
+
+    return index >= primoNumero && index <= secondoNumero
+
+});
+
+
+console.log(ultimoArray);
